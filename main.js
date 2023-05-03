e1a6b5fa5ec80a18de6962223a5817ae2a7eb675
remidodoso <joseph.nathan.hall@gmail.com>
2023-05-03 01:58:15 -0700

    Broken smooth scroll code just for the record

--- main.js
+++ main.js
@@ -53,20 +53,95 @@ var thumbs = [];
 //
 async function create_thumbnails() {
     let carousel_container = document.getElementById('carousel-container');
+
+    // I can't quite believe that it's this complex to implement a smooth scroll
+    // for the carousel, and even this doesn't have the expected behavior. So I'm
+    // going with the simple version and no smooth scroll until I've had time to
+    // ponder.
+    // I'm going to commit then delete this and come back to it :)   
+
+    /*
+    carousel_container.addEventListener('wheel', carousel_wheel, false);
+
+    let calls = 0;
+    let in_animation = false;
+    let target_scroll_left = carousel_container.scrollLeft;
+
+    function carousel_wheel(e) {
+        if (!in_animation) {
+            in_animation = true;
+            requestAnimationFrame(animate_scroll);
+        }
+
+        const delta = e.deltaY;
+        target_scroll_left += delta;
+        console.log('delta: ' + delta);
+        console.log('wheel');
+        console.log('target_scroll_left: ' + target_scroll_left);
+        const max_scroll_left = carousel_container.scrollWidth - carousel_container.clientWidth;
+        if (target_scroll_left < 0) {
+            target_scroll_left = 0;
+        } else if (target_scroll_left >= max_scroll_left) {
+            target_scroll_left = max_scroll_left;
+        }
+        console.log('target_scroll_left: ' + target_scroll_left);
+            
+
+        function animate_scroll(current_time) {
+            calls += 1;
+            console.log('animate ' + calls);
+            const start_scroll_left = carousel_container.scrollLeft;
+            p_now = performance.now();
+            console.log('current time: ' + current_time + ' p_now: ' + p_now);
+            const time_elapsed = p_now - Math.floor(current_time);
+            console.log('elapsed: ' + time_elapsed);
+            const scroll_distance = target_scroll_left - start_scroll_left;
+            console.log('animate: start_scroll_left: '+ start_scroll_left + ' scroll_distance: ' + scroll_distance);
+            const duration = Math.abs(scroll_distance) / 5;
+
+            // this function creates a smooth scroll
+            const easing = t => 
+                t < .5 ?
+                    2 * t * t :
+                    -1 + (4 - 2 * t);
+
+            console.log('elapsed: ' + time_elapsed + ' duration: ' + duration);
+            if (time_elapsed < duration) {
+                const t = time_elapsed / duration;
+                console.log('easing: ' + easing(t));
+                const scroll_left = start_scroll_left + (Math.abs(scroll_distance / 5) < 1 ? Math.sign(scroll_distance) : scroll_distance / 20);
+                console.log('scroll_left: ' + scroll_left);
+                carousel_container.scrollLeft = scroll_left;
+                requestAnimationFrame(animate_scroll);
+            } else {
+                carousel_container.scrollLeft = target_scroll_left;
+                in_animation = false;
+            }
+
+        }
+    }    
+    */
+
     for (let i = 0; i < images.length; i++) {
-        let canvas = document.createElement("canvas");
+        const canvas = document.createElement("canvas");
         canvas.dataset.imageNumber = i;
         canvas.dataset.imageSrc = images[i];
-        let divbox = document.createElement('DIV');
+        const divbox = document.createElement('DIV');
         divbox.appendChild(canvas);
         carousel_container.appendChild(divbox);
         thumbs.push(canvas);
     }  
     Array.from(carousel_container.getElementsByTagName('CANVAS')).forEach(canvas => {
-        let image = new Image();
-        let ctx = canvas.getContext('2d');
+        const image = new Image();
+        const ctx = canvas.getContext('2d');
         image.onload = function () {
-            let scale = G_THUMBNAIL_HEIGHT / image.height;
+            //
+            // TBD add some kind of provision for extreme aspect ratios
+            // Probably restrict to 3:1 or something like that (whatever
+            // 35mm pano is), lop off the extra bits symmetrically, and make the
+            // thumbnail from the remaining portion.
+            // 
+            const scale = G_THUMBNAIL_HEIGHT / image.height;
             canvas.width = image.width * scale;
             canvas.height = image.height * scale;
             Object.keys(G_THUMBNAIL_INACTIVE_STYLE).forEach(attr => {
@@ -96,7 +171,7 @@ function set_current_index(new_index) {
         if (new_index == current_index) {
             return;
         }
-        canvas = thumbs[current_index];
+        const canvas = thumbs[current_index];
         Object.keys(G_THUMBNAIL_INACTIVE_STYLE).forEach(attr => {
             canvas.style[attr] = G_THUMBNAIL_INACTIVE_STYLE[attr];
         });
@@ -109,15 +184,22 @@ function set_current_index(new_index) {
 }
 
 //
-// "lobjet" -> "l'objet d'art"
-// and now you know
+// called from <body onload ...
 //
-
 function init() {
 
+    //
+    // "lobjet" -> "l'objet d'art"
+    // and now you know
+    //
     let lobjet_down_x;
     let lobjet_pane;
 
+    //
+    // Handlers ...
+    // I should just ignore mobile for the most part, for now, as swipe is a pain,
+    // or at least painful for me to understand ...
+    //
     function lobjet_pointerdown(e) {
         if (!e.isPrimary) { returh; }
         if ((e.buttons & 1) == 1) {
@@ -154,9 +236,17 @@ function init() {
         }
         console.log(e);
     }
+
+    function lobjet_wheel(e) {
+        if (e.deltaY > 0) {
+            advance();
+        } else {
+            retreat();
+        }
+    }
     
-    let photo_el = document.getElementById('photo');
-    photo_el.onwheel = wheel;
+    const photo_el = document.getElementById('photo');
+    photo_el.onwheel = lobjet_wheel;
     document.onkeydown = keydown;
 
     //
@@ -168,10 +258,10 @@ function init() {
     lobjet_pane.addEventListener('pointerup', lobjet_pointerup, false);
     lobjet_pane.addEventListener('pointerout', lobjet_pointerout, false);
 
-    let gallery_title_el = document.getElementById('gallery-title');
+    const gallery_title_el = document.getElementById('gallery-title');
     gallery_title_el.innerHTML =
         '<span style="font-size: larger">' + gallery_title + '</span>';
-    create_thumbnails();
+    create_thumbnails(); // is async
     set_current_index(0);
 }
 
@@ -190,13 +280,6 @@ function retreat() {
     }
     set_current_index(i);
     draw();
-}
-function wheel(event) {
-    if (event.deltaY > 0) {
-        advance();
-    } else {
-        retreat();
-    }
 }
 function keydown(event) {
     if (event.key == 'ArrowRight' || event.key == 'Right') {
@@ -222,10 +305,10 @@ function torate() {
 //     and a password, thence stored on the browser
 //
 function steg(canvas) {
-    let x = canvas.width / 2;
-    let y = canvas.height / 2;
-    let ctx = canvas.getContext('2d');
-    let image_data = ctx.getImageData(x, y, 100, 1);
+    const x = canvas.width / 2;
+    const y = canvas.height / 2;
+    const ctx = canvas.getContext('2d');
+    const image_data = ctx.getImageData(x, y, 100, 1);
     for (let i = 0; i < 100; i++) {
         image_data.data[i * 4] &= 240;
         image_data.data[i * 4 + 1] &= 240;
@@ -239,16 +322,16 @@ function draw(image_number = null) {
     //
     // Draw into a hidden canvas first to avoid flicker
     //
-    let backing_canvas = document.getElementById('backing');
-    let canvas = document.getElementById('photo');
-    let backing_context;
+    const backing_canvas = document.getElementById('backing');
+    const canvas = document.getElementById('photo');
+    let backing_ctx;
     if (backing_canvas.getContext) {
         backing_ctx = backing_canvas.getContext('2d');
     }
     photo_box = document.getElementById('lobjet_pane');
     backing_canvas.width = photo_box.clientWidth;
     backing_canvas.height = photo_box.clientHeight;
-    let image = new Image();
+    const image = new Image();
 
     image.onload = function () {
         let scale = Math.min(backing_canvas.width / image.width, backing_canvas.height / image.height);
@@ -258,8 +341,11 @@ function draw(image_number = null) {
         let size = document.getElementById('size');
         size.innerHTML = '' +
             image.width + ' x ' + image.height;
-        filename = document.getElementById('filename');
+        const filename = document.getElementById('filename');
 
+        //
+        // Update: Nope to this below, blob URLs seem like the right way
+        //
         // Planning to use data URLs to create secure-ish links to stegged images.
         // This seems like it will work, but takes a few seconds or more to do so
         // it will have to be done on request for a download, or more likely, in the
@@ -274,7 +360,6 @@ function draw(image_number = null) {
             '<a download href="' + images[current_index] + '">' +
             '<span style="font-size: smaller">download ⤵️</span>' +
             '</a>';
-
 
         backing_ctx.save();
         backing_ctx.fillStyle = '#777';
@@ -291,6 +376,35 @@ function draw(image_number = null) {
         canvas.height = photo_box.clientHeight;
         ctx.drawImage(backing_canvas, 0, 0);
         steg(canvas);
+ 
+        //
+        // TBD
+        // So this is the outline of a way to do this, which works as a way to efficiently
+        // provide a download link to a canvas that has its size and attributes controlled.
+        // Obvious shortcomings: Both backing_canvas and canvas are padded with fill :)
+        //   so we need another canvas with the proper scaled and stegged image
+        // Needs a proper filename for the save file
+        // Maybe innerHTML is crude, maybe not, probably makes no difference, definitely
+        // easier to read
+        //
+        // Ultimately, this is much more like what we need, and it's already written in
+        // Rust ....
+        // https://github.com/iwanders/spread_spectrum_watermarking
+        /* 
+        canvas.toBlob((blob) => {
+            const blob_url  = URL.createObjectURL(blob);
+            filename.innerHTML =
+                '<a target="_blank" href="' + images[current_index] + '">' +
+                '<span style="font-size: smaller">view 👁️</span>' +
+                '</a> ' +
+                '<a download href="' + images[current_index] + '">' +
+                '<span style="font-size: smaller">download ⤵️</span>' +
+                '</a> ' +
+                '<a download href="' + blob_url + '">' +
+                '<span style="font-size: smaller">download ⤵️</span>' +
+                '</a>';
+        })
+        */
     }
     image.crossOrigin = "";
     image.src = images[current_index];
